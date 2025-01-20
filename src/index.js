978094cd55f3861953e9a5a9ec4595cf8fbba894
T-J05 <bernardinotoledo6@gmail.com>
2025-01-20 09:24:54 -0300

    se añadió al cors origin la direccion de svelte

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import cors from 'cors'
 const app = express()
 const port = process.env.PORT || 3002
 app.use(cors()); 
-const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5500',"http://localhost:4200"];
+const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5500',"http://localhost:4200","http://localhost:5173"];
 
 app.use(cors({
   origin: function(origin, callback) {
@@ -16,8 +16,11 @@ app.use(cors({
     } else {
       callback(new Error('No permitido por CORS')); // Bloquea la solicitud
     }
-  }
+  },
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']// Asegúrate de permitir PATCH si es necesario
+  // Si usas headers personalizados, añádelos aquí
 }));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 
